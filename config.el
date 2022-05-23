991ff8ce64f1de9409223120a82d31a1d930f12c
Harun Sheikhali <harun.sheikhali@agilebits.com>
2022-05-23 14:04:01 -0400

    updated my configuration

--- config.el
+++ config.el
@@ -1,21 +1,21 @@
 (require 'package)
 
 (add-to-list 'package-archives
-'("melpa" . "https://melpa.org/packages/") t)
+             '("melpa" . "https://melpa.org/packages/") t)
 
 (package-initialize)
 
 ;; Bootstrap
 (unless (package-installed-p 'use-package)
-(package-refresh-contents)
-(package-install 'use-package))
+  (package-refresh-contents)
+  (package-install 'use-package))
 (eval-when-compile
-(require 'use-package))
+  (require 'use-package))
 (require 'bind-key)
 (setq use-package-always-ensure t)
 
 (setq inhibit-startup-message t)
-      (menu-bar-mode -1)
+(menu-bar-mode -1)
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
 (tooltip-mode -1)
@@ -23,222 +23,274 @@
 (setq visible-bell t)
 
 (use-package gcmh
-						:diminish gcmh-mode
-						:config
-						(setq gcmh-idle-delay 5
-						gcmh-high-cons-threshold (* 16 1024 1024)) ;; this represets 16mb
-						(gcmh-mode 1))
-						(add-hook 'emacs-startup-hook
-						(lambda ()
-						(setq gc-cons-percentage 0.1)))
+  :diminish gcmh-mode
+  :config
+  (setq gcmh-idle-delay 5
+		gcmh-high-cons-threshold (* 16 1024 1024)) ;; this represets 16mb
+  (gcmh-mode 1))
 
-						(add-hook 'emacs-startup-hook
-						(lambda ()
-						(message "Emacs ready in %s with %d garbage collections."
-						(format "%.2f seconds"
-						(float-time
-						(time-subtract after-init-time before-init-time)))
-						gcs-done)))
-						(fset 'yes-or-no-p 'y-or-n-p)
-						(setq confirm-kill-emacs 'yes-or-no-p)
-						(setq frame-resize-pixelwise t)
-						(setq window-resize-pixelwise nil)
-						(setq-default truncate-lines t)
-						(setq-default tab-width 2)
-						(setq-default fill-column 80)
+(add-hook 'emacs-startup-hook
+		  (lambda ()
+			(setq gc-cons-percentage 0.1)))
 
-								;; stop emacs from creating annoying backup files..
-								(setq make-backup-files nil)
-								;; stop emacs from creating autosave files (eg: #main.go#)
-								(setq auto-save-default nil)
+(add-hook 'emacs-startup-hook
+		  (lambda ()
+			(message "Emacs ready in %s with %d garbage collections."
+					 (format "%.2f seconds"
+							 (float-time
+							  (time-subtract after-init-time before-init-time)))
+					 gcs-done)))
+(fset 'yes-or-no-p 'y-or-n-p)
 
-						;; configure path for linux
-				(when (memq window-system '(mac ns x))
-		(exec-path-from-shell-initialize))
-				(setq org-src-tab-acts-natively t)
+(setq confirm-kill-emacs 'yes-or-no-p)
+(setq frame-resize-pixelwise t)
+(setq window-resize-pixelwise nil)
+(setq-default truncate-lines t)
+(setq-default tab-width 4)
+(setq-default fill-column 80)
 
-		;; format org mode babel src blocks
-		(defun udf/my-org-tab-dwim (&optional arg)
+;; stop emacs from creating annoying backup files..
+(setq make-backup-files nil)
+;; stop emacs from creating autosave files (eg: #main.go#)
+(setq auto-save-default nil)
+
+		;; configure path for linux
+(when (memq window-system '(mac ns x))
+  (exec-path-from-shell-initialize))
+
+(setq org-src-tab-acts-natively t)
+
+;; format org mode babel src blocks
+(defun udf/my-org-tab-dwim (&optional arg)
   (interactive)
   (or (org-babel-do-key-sequence-in-edit-buffer (kbd "TAB"))
-      (org-cycle arg)))
+	  (org-cycle arg)))
 
 (define-key org-mode-map
   (kbd "<tab>") #'udf/my-org-tab-dwim)
 
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
 (set-face-attribute 'default nil :font "JetBrains Mono" :weight 'Bold :height 130)
-(set-face-attribute 'fixed-pitch nil :font "JetBrains Mono" :weight
-'Bold :height 130)
+(set-face-attribute 'fixed-pitch nil :font "JetBrains Mono" :weight 'Bold :height 130)
 
 (use-package swiper
-:ensure t)
+	:ensure t)
 
 (use-package magit
-:ensure t)
+  :ensure t)
+(use-package git-gutter
+  :ensure t
+  :init
+  (global-git-gutter-mode +1))
 
 (use-package paren
-:ensure t
-:config
-(setq show-paren-delay 0.1
-show-paren-highlight-openparen t
-show-paren-when-point-inside-paren t
-show-paren-when-point-in-periphery t)
-(show-paren-mode 1))
+  :ensure t
+  :config
+  (setq show-paren-delay 0.1
+		show-paren-highlight-openparen t
+		show-paren-when-point-inside-paren t
+		show-paren-when-point-in-periphery t)
+  (show-paren-mode 1))
 
 (use-package smartparens
-:diminish smartparens-mode
-:defer 1
-:config
-(require 'smartparens-config)
-(setq sp-max-prefix-length 25)
-(setq sp-max-pair-length 4)
-(setq sp-highlight-pair-overlay nil
-sp-highlight-wrap-overlay nil
-sp-highlight-wrap-tag-overlay nil)
-(with-eval-after-load 'evil
-(setq sp-show-pair-from-inside t)
-(setq sp-cancel-autoskip-on-backward-movement nil)
-(setq sp-pair-overlay-keymap (make-sparse-keymap)))
+  :diminish smartparens-mode
+  :defer 1
+  :config
+  (require 'smartparens-config)
+  (setq sp-max-prefix-length 25)
+  (setq sp-max-pair-length 4)
+  (setq sp-highlight-pair-overlay nil
+		sp-highlight-wrap-overlay nil
+		sp-highlight-wrap-tag-overlay nil)
+  (with-eval-after-load 'evil
+	(setq sp-show-pair-from-inside t)
+	(setq sp-cancel-autoskip-on-backward-movement nil)
+	(setq sp-pair-overlay-keymap (make-sparse-keymap)))
 
-(let ((unless-list '(sp-point-before-word-p
-sp-point-after-word-p
-sp-point-before-same-p)))
-(sp-pair "'" nil :unless unless-list))
+  (let ((unless-list '(sp-point-before-word-p
+					       sp-point-after-word-p
+					       sp-point-before-same-p)))
+	(sp-pair "'" nil :unless unless-list))
 
-(sp-local-pair sp-lisp-modes "(" ")" :unless '(:rem sp-point-before-same-p))
-(sp-local-pair '(emacs-lisp-mode org-mode markdown-mode gfm-mode)
-"[" nil :post-handlers '(:rem ("| " "SPC")))
+  (sp-local-pair sp-lisp-modes "(" ")" :unless '(:rem sp-point-before-same-p))
+  (sp-local-pair '(emacs-lisp-mode org-mode markdown-mode gfm-mode)
+				 "[" nil :post-handlers '(:rem ("| " "SPC")))
 
-(dolist (brace '("(" "{" "["))
-(sp-pair brace nil
-:post-handlers '(("||\n[i]" "RET")("| " "SPC"))
-:unless '(sp-point-before-word-p sp-point-before-same-p)))
-(smartparens-global-mode t))
+  (dolist (brace '("(" "{" "["))
+	(sp-pair brace nil
+			 :post-handlers '(("||\n[i]" "RET")("| " "SPC"))
+			 :unless '(sp-point-before-word-p sp-point-before-same-p)))
+  (smartparens-global-mode t))
+(use-package rainbow-delimiters
+  :ensure t)
+
+(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
 
 (use-package evil
-	:init
-	(setq evil-want-keybinding t)
-	(setq evil-want-fine-undo t)
-	(setq evil-want-keybinding nil)
-	:config
-	(define-key evil-motion-state-map "/" 'swiper)
-	(define-key evil-window-map "\C-w" 'evil-delete-buffer)
-	(define-key evil-motion-state-map "\C-b" 'evil-scroll-up)
+      :init
+      (setq evil-want-keybinding t)
+      (setq evil-want-fine-undo t)
+      (setq evil-want-keybinding nil)
+      :config
+      (define-key evil-motion-state-map "/" 'swiper)
+      (define-key evil-window-map "\C-w" 'evil-delete-buffer)
+      (define-key evil-motion-state-map "\C-b" 'evil-scroll-up)
 
-	;; Setting cursor colors
-	(setq evil-emacs-state-cursor '("#649bce" box))
-	(setq evil-normal-state-cursor '("#ebcb8b" box))
-	(setq evil-operator-state-cursor '("#ebcb8b" hollow))
-	(setq evil-visual-state-cursor '("#677691" box))
-	(setq evil-insert-state-cursor '("#eb998b" (bar . 2)))
-	(setq evil-replace-state-cursor '("#eb998b" hbar))
-	(setq evil-motion-state-cursor '("#ad8beb" box))
+      ;; Setting cursor colors
+      (setq evil-emacs-state-cursor '("#649bce" box))
+      (setq evil-normal-state-cursor '("#ebcb8b" box))
+      (setq evil-operator-state-cursor '("#ebcb8b" hollow))
+      (setq evil-visual-state-cursor '("#677691" box))
+      (setq evil-insert-state-cursor '("#eb998b" (bar . 2)))
+      (setq evil-replace-state-cursor '("#eb998b" hbar))
+      (setq evil-motion-state-cursor '("#ad8beb" box))
 
-	(evil-define-key nil 'custom-mode-map
+      (evil-define-key nil 'custom-mode-map
 	;;motion
 	(kbd "C-j") 'widget-forward
 	(kbd "C-k") 'widget-backwards
 	"q" 'Custom-buffer-done)
 
-	;; define lsp doc stuff
-	(evil-define-key 'normal 'lsp-ui-doc-mode
+      ;; define lsp doc stuff
+      (evil-define-key 'normal 'lsp-ui-doc-mode
 	[?K] #'lsp-ui-doc-glance)
 
-	(dolist (mode '(help-mode-map
-	calendar-mode-map
-	(evil-define-key 'motion  mode "q" 'kill-this-buffer))))
-	(evil-mode 1))
+      (dolist (mode '(help-mode-map
+				      calendar-mode-map
+				      (evil-define-key 'motion  mode "q" 'kill-this-buffer))))
+      (evil-mode 1))
 
 ;; Evil escape mode
 (use-package evil-escape
-	:config
-	(setq-default evil-escape-key-sequence "jk")
-	(setq-default evil-escape-delay 0.2)
-	(evil-escape-mode +1))
+      :config
+      (setq-default evil-escape-key-sequence "jk")
+      (setq-default evil-escape-delay 0.2)
+      (evil-escape-mode +1))
 
 (use-package evil-surround
-	:defer 2
-	:config
-	(global-evil-surround-mode 1))
+      :defer 2
+      :config
+      (global-evil-surround-mode 1))
 
 (use-package evil-snipe
-	:diminish evil-snipe-mode
-	:diminish evil-snipe-local-mode
-	:after evil
-	:config
-	(evil-snipe-mode +1))
+      :diminish evil-snipe-mode
+      :diminish evil-snipe-local-mode
+      :after evil
+      :config
+      (evil-snipe-mode +1))
+
+;; manually load commentary
+(add-to-list 'load-path "~/.emacs.d/external-packages/evil-commentary")
+(require 'evil-commentary)
+(evil-commentary-mode)
 
 (use-package projectile)
 (projectile-mode 1)
 (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
 (setq projectile-project-search-path '("~/.dev/" "~/.personal/"))
 
-(use-package helm
-:ensure
-:config
-(require 'helm-config))
+(use-package projectile-ripgrep
+      :ensure t)
 
-;; re-map some global bindings to be helm
-(global-set-key (kbd "M-x") #'helm-M-x)
-(global-set-key (kbd "C-x r b") #'helm-filtered-bookmarks)
-(global-set-key (kbd "C-x C-f") #'helm-find-files)
+(use-package ivy
+      :diminish ivy-mode
+      :config
+      (setq ivy-extra-directories nil) ;; hides . and .. directories
+      (setq ivy-initial-inputs-alist nil) ;; removes the ^ in ivy searches
+      (setq-default ivy-height 15)
+      (setq ivy-fixed-height-minibuffer t)
+      (ivy-mode 1)
 
-(helm-mode 1)
+      ;; Shows a preview of the face in counsel-describe-face
+      (add-to-list 'ivy-format-functions-alist '(counsel-describe-face . counsel--faces-format-function)))
 
-(load-theme 'dracula t)
+      ;; :general
+      ;; (general-define-key
+      ;;  :keymaps '(ivy-minibuffer-map ivy-switch-buffer-map)
+      ;;  "TAB" 'ivy-next-line
+      ;;  "S-TAB" 'ivy-previous-line))
+
+(use-package all-the-icons-ivy-rich
+      :init (all-the-icons-ivy-rich-mode 1)
+      :config
+      (setq all-the-icons-ivy-rich-icon-size 1.0))
+
+(use-package ivy-rich
+      :after (ivy)
+      :init
+      (setq ivy-rich-path-style 'abbrev)
+      (setcdr (assq t ivy-format-functions-alist) #'ivy-format-function-line)
+      :config
+      (ivy-rich-mode 1))
+
+(use-package ivy-posframe
+      :config
+      (setq ivy-posframe-display-functions-alist '((t . ivy-posframe-display))))
+
+(ivy-posframe-mode 1)
+
+(use-package counsel
+      :ensure t
+      :config
+      (setq counsel-switch-buffer-preview-virtual-buffers nil))
+
+(load-theme 'doom-wilmersdorf t)
 
 (use-package company
-:diminish company-mode
-:hook ((emacs-lisp-mode . (lambda ()
-(setq-local company-backends '(company-elisp))))
-(emacs-list-mode . company-mode))
-:init
-(add-hook 'after-init-hook 'global-company-mode)
-(setq company-minimum-prefix-length 2
-company-tooltip-limit 14
-company-tooltip-align-annotations t
-company-require-match 'never
-company-frontends
-'(company-pseudo-tooltip-frontend
-company-echo-metadata-frontend)
-company-backends '(company-capf company-files company-keywords)
-company-auto-complete nil
-company-auto-complete-chars nil
-company-debbrev-other-buffers nil
-company-debbrev-ignore-case nil
-company-debbrev-downcase nil)
-:config
-(general-define-key :keymaps 'company-active-map
-"TAB" 'company-select-next
-"S-TAB" 'company-select-previous
-"<return>" 'company-complete-selection
-"RET" 'company-complete-selection)
-(setq company-idle-delay 0.35)
-(company-tng-mode))
+		       :diminish company-mode
+		       :hook ((emacs-lisp-mode . (lambda ()
+									       (setq-local company-backends '(company-elisp))))
+				      (emacs-list-mode . company-mode))
+		       :init
+		       (add-hook 'after-init-hook 'global-company-mode)
+		       (setq company-minimum-prefix-length 2
+				     company-tooltip-limit 14
+				     company-tooltip-align-annotations t
+				     company-require-match 'never
+				     company-frontends
+				     '(company-pseudo-tooltip-frontend
+				       company-echo-metadata-frontend)
+				     company-backends '(company-capf company-files company-keywords)
+				     company-auto-complete nil
+				     company-auto-complete-chars nil
+				     company-debbrev-other-buffers nil
+				     company-debbrev-ignore-case nil
+				     company-debbrev-downcase nil)
+		       :config
+		       (general-define-key :keymaps 'company-active-map
+							       "TAB" 'company-select-next
+							       "S-TAB" 'company-select-previous
+							       "<return>" 'company-complete-selection
+							       "RET" 'company-complete-selection)
+		       (setq company-idle-delay 0.35)
+		       (company-tng-mode))
+
 (with-eval-after-load 'company
-(define-key company-active-map (kbd "RET") #'company-complete-selection))
+      (define-key company-active-map (kbd "RET") #'company-complete-selection))
 
 (use-package general
-:config
-(general-define-key
-:states '(normal motion visual)
-:keymaps 'override
-:prefix ","
-"f" '(helm-find-files :which-key "find files")
-"p" '(projectile--find-file :whick-key "Find files in the current project")
-"s" '(projectile-switch-project :which-key "Switch project")
-"b" '(helm-buffers-list :which-key "Show active buffers")))
+	:config
+	(general-define-key
+	       :states '(normal motion visual)
+	       :keymaps 'override
+	       :prefix ","
+	       "f" '(counsel-find-file :which-key "find files")
+	       "p" '(projectile--find-file :whick-key "Find files in the current project")
+	       "s" '(projectile-switch-project :which-key "Switch project")
+	       "b" '(counsel-switch-buffer :which-key "Switch buffers")))
 
 (use-package which-key
-:diminish which-key-mode
-:init
-(which-key-mode)
-(which-key-setup-minibuffer)
-:config
-(setq which-key-idle-delay 0.3))
+	:diminish which-key-mode
+	:init
+	(which-key-mode)
+	(which-key-setup-minibuffer)
+	:config
+	(setq which-key-idle-delay 0.3))
 
 (use-package lsp-mode
 	:commands (lsp lsp-deferred))
+
 (use-package lsp-ui)
 
 ;; prettier
@@ -246,26 +298,26 @@ company-debbrev-downcase nil)
 :ensure t)
 
 (use-package go-mode
-:ensure t
-:hook ((go-mode . lsp))
-:bind (:map go-mode-map
-("<f6>" . gofmt)
-("C-c 6" . gofmt))
-:config
-    (setq lsp-go-analysis
-    '((fieldalignment . t)
-    (nilness . t)
-    (unusedwrite . t)
-    (unusedparams .t)))
-(setq gofmt-command "goimports")
-(setq-default indent-tabs-mode nil)
-(setq-default tab-width 4))
+	:ensure t
+	:hook ((go-mode . lsp))
+	:bind (:map go-mode-map
+							("<f6>" . gofmt)
+							("C-c 6" . gofmt))
+	:config
+	(setq lsp-go-analysis
+				'((fieldalignment . t)
+					(nilness . t)
+					(unusedwrite . t)
+					(unusedparams .t)))
+	(setq gofmt-command "goimports")
+	(setq-default indent-tabs-mode nil)
+	(setq-default tab-width 4))
 
 (use-package typescript-mode
 	:hook (
-				 (typescript-mode . lsp)
-				 (typescript-mode . highlight-indent-guides-mode)
-				 )
+				       (typescript-mode . lsp)
+				       (typescript-mode . highlight-indent-guides-mode)
+				       )
 	:config
 	(setq-default typescript-indent-level 2))
 
@@ -288,15 +340,15 @@ company-debbrev-downcase nil)
 
 
 (use-package web-mode
-	:hook (
-				 (web-mode . harun/webmode-hook)
-				 (web-mode . lsp)
-				 (css-mode . lsp)
-				 (scss-mode . lsp)
-				 )
+      :hook (
+		     (web-mode . harun/webmode-hook)
+		     (web-mode . lsp)
+		     (css-mode . lsp)
+		     (scss-mode . lsp)
+		     )
 	:commands (web-mode)
 	:mode (("\\.tsx\\'" . web-mode)
-				 ("\\.html\\'" . web-mode)))
+		       ("\\.html\\'" . web-mode)))
 
 (use-package flycheck)
 (add-hook 'after-init-hook #'global-flycheck-mode)
@@ -336,10 +388,24 @@ company-debbrev-downcase nil)
 	(org-superstar-leading-bullet "")
 	)
 
-(use-package spaceline
-				:ensure t
-				:config
-				(require 'spaceline-config)
-		(spaceline-spacemacs-theme)
-		(setq powerline-default-separator 'arrow)
-(spaceline-compile))
+;; (use-package spaceline
+;;   :ensure t
+;;   :config
+;;   (require 'spaceline-config)
+;;   (spaceline-spacemacs-theme)
+;;   (setq powerline-default-separator 'box)
+;;   (spaceline-compile))
+
+;; look into using doom-modeline?
+(use-package doom-modeline
+      :init (doom-modeline-mode)
+      :config
+      (setq doom-modeline-modal-icon nil))
+
+(add-to-list 'load-path "~/.emacs.d/external-packages/snails") ; add snails to your load-path
+(require 'snails)
+
+(use-package yasnippet
+      :init (yas-global-mode 1))
+
+;; TODO: make sure I install all the other plugins for yas-snippets
